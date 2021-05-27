5a48c69e99c3f306a58e682cacbceee958d2109a
Gabriel Smith <gabrielsmith@smith-air.local>
2021-05-27 23:01:21 +0900

    remove absolute path to git in dotfile setup and clone scripts

--- dotfiles-git-clone.sh
+++ dotfiles-git-clone.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/zsh
 git clone --bare https://github.com/gabrielsmith/dotfiles $HOME/.cfg
-alias dotfiles='/usr/local/bin/git --git-dir=$HOME/.files/ --work-tree=$HOME'
+alias dotfiles='git --git-dir=$HOME/.files/ --work-tree=$HOME'
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
--- dotfiles-git-setup.sh
+++ dotfiles-git-setup.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 git init --bare $HOME/.files
-alias dotfiles='/usr/local/bin/git --git-dir=$HOME/.files/ --work-tree=$HOME'
+alias dotfiles='git --git-dir=$HOME/.files/ --work-tree=$HOME'
 dotfiles config status.showUntrackedFiles no
