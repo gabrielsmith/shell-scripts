d7cecd969ed983048ecfca4b9623cc3bfa0e7b07
Gabriel Smith <gabrielsmith@smith-air.local>
2021-05-27 23:25:17 +0900

    fix error where .files folder would be added to local .gitignore file and not global one in HOME dir

--- dotfiles-git-clone.sh
+++ dotfiles-git-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/zsh
 git clone --bare https://github.com/gabrielsmith/dotfiles $HOME/.files
-echo ".files" >> .gitignore
+echo ".files" >> $HOME/.gitignore
 alias dotfiles='git --git-dir=$HOME/.files/ --work-tree=$HOME'
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
